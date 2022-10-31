2daac8ff9fa77de9a2d9e9af509f335b0ac48e49
초연 <tgmyus@gmail.com>
2022-10-31 18:39:42 +0900

    Edit: Log Macro

--- GumaEngine/Source/Runtime/Core/Public/Log.h
+++ GumaEngine/Source/Runtime/Core/Public/Log.h
@@ -1,9 +1,10 @@
 #pragma once
 
+#include "Core.h"
+
 #include <memory>
 #include <spdlog/spdlog.h>
-
-#include "Core.h"
+#include <spdlog/fmt/ostr.h>
 
 namespace Guma
 {
@@ -22,13 +23,15 @@ namespace Guma
 }
 
 #define GUMA_ENGINE_TRACE(...) ::Guma::Log::GetEngineLogger()->trace(__VA_ARGS__)
+#define GUMA_ENGINE_DEBUG(...) ::Guma::Log::GetEngineLogger()->debug(__VA_ARGS__)
 #define GUMA_ENGINE_INFO(...)  ::Guma::Log::GetEngineLogger()->info(__VA_ARGS__)
 #define GUMA_ENGINE_WARN(...)  ::Guma::Log::GetEngineLogger()->warn(__VA_ARGS__)
 #define GUMA_ENGINE_ERROR(...) ::Guma::Log::GetEngineLogger()->error(__VA_ARGS__)
-#define GUMA_ENGINE_FATAL(...) ::Guma::Log::GetEngineLogger()->fatal(__VA_ARGS__)
+#define GUMA_ENGINE_CRITICAL(...) ::Guma::Log::GetEngineLogger()->critical(__VA_ARGS__)
 
 #define GUMA_APP_TRACE(...) ::Guma::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define GUMA_APP_DEBUG(...) ::Guma::Log::GetClientLogger()->debug(__VA_ARGS__)
 #define GUMA_APP_INFO(...)  ::Guma::Log::GetClientLogger()->info(__VA_ARGS__)
 #define GUMA_APP_WARN(...)  ::Guma::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define GUMA_APP_ERROR(...) ::Guma::Log::GetClientLogger()->error(__VA_ARGS__)
-#define GUMA_APP_FATAL(...) ::Guma::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define GUMA_APP_CRITICAL(...) ::Guma::Log::GetClientLogger()->critical(__VA_ARGS__)
